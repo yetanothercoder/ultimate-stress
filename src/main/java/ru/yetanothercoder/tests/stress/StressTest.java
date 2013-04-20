63934477823f14cfd0cc02d8f0cbb07c177803a6
mbaturov <mbaturov@gmail.com>
2013-04-20 11:16:47 +0400

    added main project

---
+++ src/main/java/ru/yetanothercoder/tests/stress/StressTest.java
@@ -0,0 +1,10 @@
+package ru.yetanothercoder.tests.stress;
+
+/**
+ * @author Mikhail Baturov,  4/20/13 11:11 AM
+ */
+public class StressTest {
+    public static void main(String[] args) {
+        System.out.println("!");
+    }
+}
