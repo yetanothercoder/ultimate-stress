db6966ce3b1ee0e0e0b57574591ead09ed596eef
mbaturov <mbaturov@gmail.com>
2013-04-22 00:41:21 +0400

    test push

--- src/main/java/ru/yetanothercoder/tests/stress/StressTest.java
+++ src/main/java/ru/yetanothercoder/tests/stress/StressTest.java
@@ -1,12 +1,13 @@
 package ru.yetanothercoder.tests.stress;
 
 import org.jboss.netty.bootstrap.Bootstrap;
+import org.jboss.netty.bootstrap.ClientBootstrap;
 
 /**
  * @author Mikhail Baturov,  4/20/13 11:11 AM
  */
 public class StressTest {
     public static void main(String[] args) {
-        Bootstrap b = null;
+        Bootstrap b = new ClientBootstrap();
     }
 }
